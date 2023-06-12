0168a374d6e8e74357894caa85cac3c2e480172b
69codes <taiwoyusuf24@gmail.com>
2023-06-12 15:55:33 +0100

    Testing

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="header">
+    <nav class="navbar">
+      <div class="logo">My Website</div>
+      <div class="links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+      </div>
+      <button class="toggle-button" @click="toggleMenu">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </button>
+    </nav>
+    <div v-show="showMenu" class="mobile-menu">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Services</a>
+      <a href="#">Contact</a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.showMenu = !this.showMenu;
+    },
+  },
+};
+</script>
+
+
---
+++ src/components/SignUp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="signup-page">
+    <h1 class="title">Sign Up</h1>
+    <form class="signup-form">
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" id="name" v-model="name" />
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model="email" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <button class="signup-button" @click="submitForm">Sign Up</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      // Handle form submission
+    },
+  },
+};
+</script>
+
+
